79dc3d732081b28e803d107b8a820b0afa213143
elguesabal <joseelguesabal@gmail.com>
2026-02-11 20:21:30 -0300

    testanto o comando 'npm run build' na vercel

--- src/route/chat/messages/Messages.jsx
+++ src/route/chat/messages/Messages.jsx
@@ -69,7 +69,7 @@ function Message({ message }) {
  * @brief MENSAGENS DO CHAT
  * @param {Object} socket SOCKET DE CONEXAO COM O BACK END
 */
-export default function Messages({ socket }) {
+export default function Messages({ socket }) {					// TESTANDO
 	const [error, setError] = useState(false);
 	const [messages, setMessages] = useState(null);
 	const bottomRef = useRef(null);
