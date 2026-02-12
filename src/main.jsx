5d55b5cdad2a6b12e7f6dbf970e9a38539cdea60
elguesabal <joseelguesabal@gmail.com>
2026-02-12 15:22:46 -0300

    tetando corrigir o erro de icone na vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./style/index.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import Chat from "./route/chat/Chat.jsx";
 
 createRoot(document.getElementById("root")).render(
