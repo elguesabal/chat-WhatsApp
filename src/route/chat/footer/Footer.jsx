e3690c97b0a79fede0e2dbe3ea86d98baa5e229b
elguesabal <joseelguesabal@gmail.com>
2026-02-18 20:24:24 -0300

    tentando fazer o botao de enviar mensagem mas falhando (o codigo esta comentado)

--- src/route/chat/footer/Footer.jsx
+++ src/route/chat/footer/Footer.jsx
@@ -1,16 +1,25 @@
 import { useState, useEffect } from "react";
 
+// function teste(socket, message) {
+// alert(message)
+// console.log(socket)
+// 	socket.emit("send_message_text", { text: message }, (res) => {
+// console.log(res)
+// 	});
+// }
+
 /**
  * @author VAMPETA
  * @brief COMPONENTE RESPONSAVEL POR GERAR O CAMPO DE DIGITACAO DA MENSAGEM E BOTAO DE ENVIAR
  * @param {Object} socket SOCKET DE CONEXAO COM O BACK END
 */
-function ChatComposer() {
+function ChatComposer(socket) {
 	const [message, setMessage] = useState("");
 
 	return (
 		<div className="flex items-center justify-center h-14 bg-gray-800 shrink-0">
 			<textarea className="px-2 h-12 leading-[3rem] resize-none bg-orange-100 rounded" rows={1} value={message} onChange={(e) => setMessage(e.target.value)} />
+			{/* <button onClick={() => teste(socket, message)}> */}
 			<button onClick={() => alert(message)}>
 				<i className="bi bi-send text-2xl text-orange-500" />
 			</button>
@@ -45,7 +54,7 @@ export default function Footer({ socket }) {
 		case null:
 			return (<></>);
 		case true:
-			return (<ChatComposer />);
+			return (<ChatComposer socket={socket} />);
 		case false:
 			return (
 				<div className="flex items-center justify-center h-14 bg-gray-800 shrink-0">
